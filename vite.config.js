18784a49f0a7722d2875ab3d4324f14a0a5dd525
JesusClase <dayekhmartinezsebastianjesus@fpllefia.com>
2025-01-22 20:22:58 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  publicDir: path.resolve(__dirname, 'public'),
-  build: {
-    outDir: path.resolve(__dirname, 'dist'), // Define el directorio de salida
-    emptyOutDir: true, // Limpia el directorio antes de cada build
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+ root: path.resolve(__dirname, 'src'),
+ publicDir: path.resolve(__dirname, 'public'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
-  },
 });
